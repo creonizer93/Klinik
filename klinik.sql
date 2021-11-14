8b1f613a57593ff7a858e4c5900e589691a1ad35
creonizer93 <creonizer123@gmail.com>
2021-11-14 11:43:28 +0800

    Update klinik.sql
    update umur

--- klinik.sql
+++ klinik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Nov 04, 2021 at 04:37 PM
+-- Generation Time: Nov 14, 2021 at 03:42 AM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -60,9 +60,13 @@ INSERT INTO `lab` (`id`, `nama`, `satuan`, `harga`, `created_time`, `updated_tim
 (1, 'Kolesterol', 'mg/dl', 15000, '2020-04-07 08:32:20', '2020-04-12 03:05:59', 1),
 (2, 'Asam Urat', 'mg/dl', 15000, '2020-04-07 01:43:20', '2020-04-07 01:43:20', 0),
 (3, 'Gula Darah Sewaktu', 'mg/dl', 15000, '2020-04-07 01:43:47', '2020-04-07 01:51:29', 0),
-(4, 'Gula Darah Puasa', 'mg/dl', 15000, '2020-04-12 03:06:27', '2020-04-12 03:06:27', 0),
+(4, 'Gula Darah Puasa', 'mg/dl', 15000, '2020-04-12 03:06:27', '2021-11-13 18:12:49', 0),
 (5, 'Gula Darah 2 Jam PP', 'mg/dl', 15000, '2020-04-26 05:09:37', '2020-04-26 21:05:34', 0),
-(6, 'Hemoglobin', 'mg/dl', 15000, '2020-04-26 05:45:42', '2020-04-26 05:45:42', 1);
+(6, 'Hemoglobin', 'mg/dl', 15000, '2020-04-26 05:45:42', '2020-04-26 05:45:42', 1),
+(8, 'kolestrol', 'g/dl', 25000, '2021-11-13 18:13:31', '2021-11-13 18:13:31', 0),
+(9, 'Hemoglobin', 'g/dl', 20000, '2021-11-13 18:14:14', '2021-11-13 18:14:14', 0),
+(10, 'Injeksi', 'g/dl', 30000, '2021-11-13 18:14:42', '2021-11-13 18:14:42', 0),
+(11, 'Dresing Luka', 'g/dl', 30000, '2021-11-13 18:15:06', '2021-11-13 18:15:06', 0);
 
 -- --------------------------------------------------------
 
@@ -98,7 +102,8 @@ INSERT INTO `metadata` (`id`, `Judul`, `Deskripsi`) VALUES
 (15, 'Lihat rekam Medis', 'Lihat Rekam Medis Pasien Yang Dipilih'),
 (16, 'Pengaturan', 'Pengaturan yang tersedia untuk klinik anda'),
 (17, 'Dashboard', 'Halaman muka dari klinik anda, overview hal-hal mengenai klinik anda.'),
-(18, 'Daftar Pengguna', 'Daftar pengguna atau pegawai yang dapat log-in di klinik anda.');
+(18, 'Daftar Pengguna', 'Daftar pengguna atau pegawai yang dapat log-in di klinik anda.'),
+(19, 'Antri Rekam Medis', 'Antrian pasien yang hadir di klinik');
 
 -- --------------------------------------------------------
 
@@ -144,12 +149,12 @@ CREATE TABLE `obat` (
 --
 
 INSERT INTO `obat` (`id`, `nama_obat`, `sediaan`, `dosis`, `satuan`, `stok`, `harga`, `created_time`, `updated_time`, `deleted`) VALUES
-(1, 'Metronidazole', 'Tablet', 500, 'mg', 100, 10000, '2020-04-26 19:00:17', '2021-11-01 19:12:11', 0),
-(2, 'Amoxicillin', 'Tablet', 500, 'mg', 88, 10000, '2020-04-26 19:00:52', '2020-04-26 19:00:52', 0),
-(3, 'Cefixime', 'Kapsul', 200, 'mg', 96, 40000, '2020-04-26 12:04:38', '2020-04-26 12:04:38', 0),
+(1, 'Metronidazole', 'Tablet', 500, 'mg', 100, 10000, '2020-04-26 19:00:17', '2021-11-04 13:40:40', 0),
+(2, 'Amoxicillin', 'Tablet', 500, 'mg', 53, 10000, '2020-04-26 19:00:52', '2020-04-26 19:00:52', 0),
+(3, 'Cefixime', 'Kapsul', 200, 'mg', 100, 40000, '2020-04-26 12:04:38', '2020-04-26 12:04:38', 0),
 (4, 'Cefixime', 'Kapsul', 100, 'mg', 100, 30000, '2020-04-26 12:05:19', '2020-04-26 12:05:19', 1),
-(5, 'Paracetamol', 'Tablet', 500, 'mg', 80, 10000, '2020-04-26 12:08:32', '2020-04-26 12:08:32', 0),
-(6, 'Tanpa Obat', 'Tablet', 0, 'g', 99999, 0, '2021-11-02 19:27:51', '2021-11-02 19:27:51', 0);
+(5, 'Paracetamol', 'Tablet', 500, 'mg', 92, 10000, '2020-04-26 12:08:32', '2020-04-26 12:08:32', 0),
+(6, 'Lapifed', 'Tablet', 60, 'mg', 88, 30000, '2021-11-01 14:53:41', '2021-11-01 14:53:41', 0);
 
 -- --------------------------------------------------------
 
@@ -159,30 +164,29 @@ INSERT INTO `obat` (`id`, `nama_obat`, `sediaan`, `dosis`, `satuan`, `stok`, `ha
 
 CREATE TABLE `pasien` (
   `id` int(4) NOT NULL,
+  `ktp` varchar(16) DEFAULT NULL,
   `nama` varchar(40) NOT NULL,
-  `tgl_lhr` date NOT NULL,
+  `umur` varchar(3) DEFAULT NULL,
   `jk` varchar(10) NOT NULL,
   `alamat` text NOT NULL,
-  `hp` varchar(14) NOT NULL,
+  `hp` varchar(14) DEFAULT NULL,
   `pendidikan` varchar(16) DEFAULT NULL,
-  `pekerjaan` varchar(20) NOT NULL,
-  `no_bpjs` int(15) DEFAULT NULL,
+  `pekerjaan` varchar(20) DEFAULT NULL,
+  `no_bpjs` varchar(15) DEFAULT NULL,
   `alergi` text,
   `created_time` datetime NOT NULL,
   `updated_time` datetime NOT NULL,
-  `deleted` tinyint(1) DEFAULT '0'
+  `deleted` tinyint(1) DEFAULT '0',
+  `visit` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `pasien`
 --
 
-INSERT INTO `pasien` (`id`, `nama`, `tgl_lhr`, `jk`, `alamat`, `hp`, `pendidikan`, `pekerjaan`, `no_bpjs`, `alergi`, `created_time`, `updated_time`, `deleted`) VALUES
-(1, 'Jajang Rukmana Sukarna', '2020-04-01', 'Perempuan', 'JAKARTA', '082191019181', 'SMP', 'Buruh', 9182717, 'tidak ada alergi', '0000-00-00 00:00:00', '2020-04-27 03:48:16', 0),
-(2, 'Abdul Somara', '1991-01-01', 'Laki-laki', 'Garut indah sekali jaya tentrem abadi dan tak terlupakan', '0918212111', NULL, 'Pengangguran', 1092811221, 'alergi kamu', '0000-00-00 00:00:00', '2020-04-27 03:58:25', 0),
-(6, 'Pinkan Rambo', '1991-02-01', 'perempuan', 'Hutan', '019281992', 'Tidak Ssekolah', 'Model', NULL, NULL, '2020-04-27 04:01:21', '2020-04-27 04:01:21', 0),
-(7, 'Fia Jatuh', '1991-01-01', 'Laki-laki', 'Panggung', '01999212', 'Tidak Sekolah', 'soundsystem', 12312124, 'kadada', '2020-04-27 04:05:21', '2021-11-01 19:02:45', 0),
-(8, 'Pebryanur Rusuan', '1993-02-20', 'Laki-laki', 'Jl. hm nurung', '08115002833', 'Perguruan Tinggi', 'pemecah batu', 125465465, 'Alergi Debu', '2021-11-02 18:03:15', '2021-11-02 18:03:15', 0);
+INSERT INTO `pasien` (`id`, `ktp`, `nama`, `umur`, `jk`, `alamat`, `hp`, `pendidikan`, `pekerjaan`, `no_bpjs`, `alergi`, `created_time`, `updated_time`, `deleted`, `visit`) VALUES
+(15, '6310631063106310', 'pebryan', '28', 'Laki-laki', 'banjarmasin', '08115002833', NULL, NULL, NULL, NULL, '2021-11-14 01:32:08', '2021-11-14 01:37:20', 0, 0),
+(16, '6310631063106310', 'akbar', '25', 'Laki-laki', 'JL. HM NURUNG PAGATAN', '08120808080', NULL, NULL, NULL, NULL, '2021-11-14 02:20:32', '2021-11-14 02:20:32', 0, 1);
 
 -- --------------------------------------------------------
 
@@ -205,7 +209,7 @@ CREATE TABLE `pengaturan` (
 --
 
 INSERT INTO `pengaturan` (`id`, `n_Klinik`, `Slogan`, `logo`, `gambarbool`, `gambar`, `jasa`) VALUES
-(1, 'Linx Klinik', 'Melayani Sepenuh Hati', 'fa-plus', 0, 'logo1587958142.png', 100000);
+(1, 'Arriza Medica', 'Sehatmu, Ibadahku', 'fas fa-stethoscope', 0, 'logo1587958142.png', 100000);
 
 -- --------------------------------------------------------
 
@@ -218,7 +222,13 @@ CREATE TABLE `rm` (
   `idpasien` int(4) NOT NULL,
   `ku` varchar(40) NOT NULL,
   `anamnesis` text NOT NULL,
-  `pxfisik` text NOT NULL,
+  `pxfisik` text,
+  `sistolik` varchar(3) DEFAULT NULL,
+  `diastolik` varchar(3) DEFAULT NULL,
+  `hr` varchar(3) DEFAULT NULL,
+  `bb` varchar(3) DEFAULT NULL,
+  `tb` varchar(3) DEFAULT NULL,
+  `rr` varchar(3) DEFAULT NULL,
   `lab` text,
   `hasil` text,
   `diagnosis` varchar(40) DEFAULT NULL,
@@ -228,29 +238,16 @@ CREATE TABLE `rm` (
   `dokter` int(3) NOT NULL,
   `created_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `deleted` tinyint(1) DEFAULT '0'
+  `deleted` tinyint(1) DEFAULT '0',
+  `visit` tinyint(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `rm`
 --
 
-INSERT INTO `rm` (`id`, `idpasien`, `ku`, `anamnesis`, `pxfisik`, `lab`, `hasil`, `diagnosis`, `resep`, `jumlah`, `aturan`, `dokter`, `created_time`, `updated_time`, `deleted`) VALUES
-(1, 1, 'Pusing', 'Demam 10 hari', 'T:38.7', '3|1|2', '150|260|10', 'Febris', '1|2', '1|6', '3x1|3x1', 2, '2020-04-11 12:26:51', '2020-04-26 13:32:15', 0),
-(3, 1, 'Pegal', 'Pegal Linu', 'Nyeri tekan di otot biceps', '', '', 'Myalgia', '1', '2', '2x1', 4, '2020-04-12 09:31:03', '2020-04-26 05:51:40', 0),
-(5, 2, 'lemas', 'Sakit', 'Normal', '', '', 'Typhoid Fever', '2', '10', '2x1', 2, '2020-04-26 05:56:01', '2020-04-26 05:56:01', 0),
-(6, 1, 'asdfasdf', 'asdfasd', 'fasdfasdf', '5', '60', 'asdfasdf', '5', '10', '3x1', 2, '2021-11-01 18:36:43', '2021-11-01 18:36:43', 0),
-(7, 7, 'yyyyyyyyyyy', 'yyyyyyyyyyy', 'yyyyyyyyyyy', '5', '123', 'yyyyyyyyyyy', '5|2', '1|1', '3x1|3x1', 2, '2021-11-02 17:58:30', '2021-11-02 17:58:30', 0),
-(8, 6, 'xxxxxxxxxxxxxxxx', 'xxxxxxxxxxxxxxxx', 'xxxxxxxxxxxxxxxx', '5|3', '120|120', 'xxxxxxxxxxxxxxxx', '3|5', '1|1', '3x1|2x1', 10, '2021-11-02 18:00:46', '2021-11-02 18:00:46', 0),
-(9, 8, 'sakit kepala', 'sakit dibagian kepala', 'dasar bujur', '', '', 'banyak tekanan hidup', '5', '1', '3x1', 2, '2021-11-02 18:04:58', '2021-11-02 18:04:58', 0),
-(10, 2, 'jabanterbang', 'jabanterbang', 'jabanterbang', '', '', 'jabanterbang', 'Tanpa Resep', '', '', 2, '2021-11-02 19:18:42', '2021-11-02 21:40:15', 0),
-(11, 2, 'tanpa resep', 'tanpa resep', 'tanpa resep', '', '', 'tanpa resep', '6', '0', '0', 2, '2021-11-02 20:07:52', '2021-11-02 20:07:52', 0),
-(12, 1, 'tanpa recipe', 'tanpa recipe', 'tanpa recipe', '', '', 'tanpa recipe', 'Tanpa Resep', '', '', 2, '2021-11-02 20:59:49', '2021-11-02 20:59:49', 0),
-(13, 2, 'tanpa recipe1', 'tanpa recipe1', 'tanpa recipe1', '', '', 'tanpa recipe1', 'Tanpa Resep', '', '', 2, '2021-11-02 21:03:02', '2021-11-02 21:03:02', 0),
-(14, 8, 'Resep `', 'Resep `', 'Resep `', '', '', 'Resep `', 'Tanpa Resep', '', '', 2, '2021-11-02 21:08:13', '2021-11-02 21:19:33', 0),
-(15, 8, 'aswarakwar', 'aswarakwar', 'aswarakwar', '', '', 'aswarakwar', 'Tanpa Resep', '', '', 10, '2021-11-02 22:42:44', '2021-11-02 22:42:44', 0),
-(16, 1, 'palui kosong', 'palui kosong', 'palui kosong', '', '', 'palui kosong', 'Tanpa Resep', '', '', 10, '2021-11-02 23:18:12', '2021-11-02 23:18:12', 0),
-(17, 1, 'mual pusing', 'mual pusing', 'mual pusing', '', '', 'mual pusing', 'Tanpa Resep', '', '', 10, '2021-11-02 23:41:26', '2021-11-02 23:45:25', 0);
+INSERT INTO `rm` (`id`, `idpasien`, `ku`, `anamnesis`, `pxfisik`, `sistolik`, `diastolik`, `hr`, `bb`, `tb`, `rr`, `lab`, `hasil`, `diagnosis`, `resep`, `jumlah`, `aturan`, `dokter`, `created_time`, `updated_time`, `deleted`, `visit`) VALUES
+(35, 16, 'keluhan akbar', 'anamnesis akbar1', NULL, '155', '62', '81', '57', '37', '38', '8', '100', 'sakit tenggorokan', '5|6', '1|1', '3x1|3x1', 4, '2021-11-14 03:02:09', '2021-11-14 03:26:59', 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -279,12 +276,13 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `profesi`, `name`, `email`, `email_verified_at`, `password`, `avatar`, `remember_token`, `admin`, `created_at`, `updated_at`, `deleted`) VALUES
-(2, 'admin', 'Dokter', 'Reza Irfan Raditya', 'rezaraditya21@gmail.com', NULL, '$2y$10$qoCFUUwn1x8o6jhYmvn92.kAgwx8OitCvc47qmtf/WFYoCGzm133K', 'avatar1587795209.jpg', NULL, 1, '2020-04-22 02:54:12', '2021-11-01 11:13:13', 0),
-(4, 'dokter', 'Dokter', 'Irfan', 'reza@linx.com', NULL, '$2y$10$GWj0qd8EpKugu4ji56nN7.iOanxOVneq4w3Lq4i11iS2uS6pYGoqK', 'default.jpg', NULL, 0, '2020-04-22 21:43:07', '2020-04-24 22:43:18', 0),
+(2, 'reza', 'Dokter', 'Bachrul Alam Arriza', 'dr_reza@gmail.com', NULL, '$2y$10$lsIzdwxI18oSYwTJVMNDd.QqN3lsbI6M3B0VhJzl9BEasd8cSIbUK', 'avatar1636800316.png', NULL, 1, '2020-04-22 02:54:12', '2021-11-13 15:45:16', 0),
+(4, 'dokter', 'Dokter', 'Akbar', 'akbar@mail.com', NULL, '$2y$10$jzR7zuPiNMMSdq9BMoffYe8zXGLuGCb4CuxLXRvksClUjmnuhq/L6', 'default.jpg', NULL, 0, '2020-04-22 21:43:07', '2021-11-12 03:07:29', 0),
 (5, NULL, 'Staff', 'Alek Kelek', NULL, NULL, '$2y$10$NvOWdrlhoHpf31D/uXUt..hT6U5.m1RY6lhdcW/hIeZpkWj.oUdPu', 'default.jpg', NULL, 0, '2020-04-23 19:05:08', '2020-04-26 21:17:38', 1),
 (6, NULL, 'Staff', 'test', NULL, NULL, '$2y$10$DcxoUIpnCtZluFcAZgDnvujKQM2X9lT0Ga4oTgks6zxFZJnryIG/K', 'avatar1587712752.jpg', NULL, 1, '2020-04-24 00:19:12', '2020-04-26 21:17:16', 1),
-(9, 'staff', 'Staff', 'Raditya', 'staff@Raditya.com', NULL, '$2y$10$KLpJyGVF3n7p7rAKv17iL.wHauTIgm/HYLoReHAfX5FJR5MSZ5AQC', 'avatar1587961527.jpg', NULL, 0, '2020-04-24 22:40:12', '2020-04-26 21:33:32', 0),
-(10, 'gadungan', 'Dokter', 'dokter gadungan', 'gadungan@mail.com', NULL, '$2y$10$TKXm7jmhyuHmfE5LPKqaK.UesX5r4AxMlLZFM1oafasFyeGgsfuve', 'default.jpg', NULL, 0, '2021-11-01 10:50:07', '2021-11-01 10:50:07', 0);
+(9, 'staff', 'Staff', 'Raditya', 'staff@Raditya.com', NULL, '$2y$10$b0Dbh1krpafSVnDGxtkLDeFhWHHK7QdoFZtmvcVAKcvY72jRF3on2', 'avatar1587961527.jpg', NULL, 0, '2020-04-24 22:40:12', '2021-11-12 03:07:51', 0),
+(10, NULL, 'Staff', 'creonizer', NULL, NULL, '$2y$10$.LXSf5/XOZe2PdvW3sRQ5efim0j8GnHxpGM9w8AUdLiEPRf72HyCi', 'avatar1636033891.jpg', NULL, 1, '2021-11-01 07:09:31', '2021-11-13 18:16:57', 1),
+(11, 'staff-admin', 'Staff', 'Staff-Admin', 'passnya_adminadmin@mail.com', NULL, '$2y$10$fd9sRiGHVXLz6fVNv3Gr5eR4bNzPJD9yDYFPEJA4E.q.zg1ZfVWJq', 'default.jpg', NULL, 1, '2021-11-13 18:18:43', '2021-11-13 18:18:43', 0);
 
 --
 -- Indexes for dumped tables
@@ -360,13 +358,13 @@ ALTER TABLE `failed_jobs`
 -- AUTO_INCREMENT for table `lab`
 --
 ALTER TABLE `lab`
-  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `metadata`
 --
 ALTER TABLE `metadata`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `migrations`
@@ -384,19 +382,19 @@ ALTER TABLE `obat`
 -- AUTO_INCREMENT for table `pasien`
 --
 ALTER TABLE `pasien`
-  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `rm`
 --
 ALTER TABLE `rm`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
